ed7b8eacf2047fed803e87ae00672b2fb4f6a084
ClaPascarella <claudiopascarella1996@gmail.com>
2024-05-24 00:56:40 +0200

    file vite

---
+++ vite-comics/src/App.vue
@@ -0,0 +1,28 @@
+<script>
+import Theheader from './components/Theheader.vue'
+
+export default {
+  components: {
+    Theheader
+  },
+
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+    //vanno qui, al solito
+  },
+  mounted() {
+    console.log("App montata");
+  }
+}
+</script>
+
+
+<template>
+
+</template>
+
+<style></style>
---
+++ vite-comics/src/components/Theheader.vue
@@ -0,0 +1,20 @@
+<script>
+
+export default {
+    data() {
+        return {
+            msg: "Sono il componente figlio!"
+        }
+    }
+}
+
+</script>
+
+<template>
+
+    <h1>Benvenuto in {{ nomeApplicazione }}!</h1>
+    <HelloWorld />
+
+</template>
+
+<style></style>
